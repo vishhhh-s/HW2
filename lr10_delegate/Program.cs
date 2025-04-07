7d4814ede6ac5af7772be492cf9ccd8a62678eec
Maria <wmasha2006@gmail.com>
2025-04-07 18:17:28 +0300

    Add project files.

---
+++ lr10_delegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lr10_delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
